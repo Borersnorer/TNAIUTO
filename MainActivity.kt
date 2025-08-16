e0ecd3f998171166539e07b6eb681d37eb171634
Borersnorer <npedrag123@gmail.com>
2025-08-16 10:49:52 +0100

    Create MainActivity.kt
    Launches the map and handles UI interactions

---
+++ MainActivity.kt
@@ -0,0 +1,18 @@
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.map_container, MapFragment())
+            .commit()
+
+        val drawButton = findViewById<Button>(R.id.btn_draw_area)
+        drawButton.setOnClickListener {
+            val fragment = supportFragmentManager.findFragmentById(R.id.map_container)
+            if (fragment is MapFragment) {
+                fragment.enableDrawingMode()
+            }
+        }
+    }
+}
