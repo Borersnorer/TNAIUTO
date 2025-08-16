7da28b5f1a26b39350e2bdca16e55619e3329d2a
Borersnorer <npedrag123@gmail.com>
2025-08-16 10:59:37 +0100

    Update LocationDatabase.kt

--- LocationDatabase.kt
+++ LocationDatabase.kt
@@ -1,4 +1,15 @@
+
 @Database(entities = [LocationEntity::class], version = 1)
 abstract class LocationDatabase : RoomDatabase() {
     abstract fun locationDao(): LocationDao
+}
+// Add this to initialize the DB
+companion object {
+    fun getInstance(context: Context): LocationDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            LocationDatabase::class.java,
+            "location_db"
+        ).build()
+    }
 }
